78d521c063c9f5fe71268afdbaa0dedfb7087433
hk <764432054@qq.com>
2019-04-13 10:19:28 +0800

    thread.c

---
+++ linuxc/code/thread/thread.c
@@ -0,0 +1,132 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+#define SECON_PTH_NUMS 2
+#define PTHEXIT -1
+
+typedef struct pthread_arg{
+	pthread_t tid;
+	int pthno;
+	int fd;
+}ptharg;
+
+//全局变量定义后，默认初始化为0
+struct global_va{
+	ptharg pth_arg[SECON_PTH_NUMS]; //结构体数组，每个元素会被当做参数传递给对应的次线程
+	int pth_exit_flag[SECON_PTH_NUMS];//退出的状态
+	pthread_attr_t attr; //存放线程新属性
+} glbva;
+
+void print_err(char *str,int line,int err_no){
+	printf("%d,%s:%s\n",line,str,strerror(err_no) );
+	exit(-1);
+}
+
+//线程退出处理函数
+
+void pth_exit_deal(void *arg){
+	pthread_t tid=((ptharg *)arg)->tid;
+	printf("pthread %lu exit!\n",tid );
+}
+void *pth_fun(void *pth_arg){
+	//pthread_detach(pthread_self());//在次线程里分离
+	//注册线程处理函数
+	pthread_cleanup_push(pth_exit_deal,pth_arg);
+
+	int fd= ((ptharg *) pth_arg)->fd;
+	int pthno=((ptharg *) pth_arg)->pthno;
+
+	pthread_t tid = ((ptharg *) pth_arg)->tid;
+	
+	while(1){
+		printf("pthno=%d,pthread_id=%lu\n",pthno,tid);
+		write(fd,"php",3);
+		write(fd,"java\n",5);
+		sleep(1);
+		//检测退出状态
+		if(glbva.pth_exit_flag[pthno] == PTHEXIT){
+			printf("pthno %d exit\n",pthno );
+			break;
+		} 
+	}
+	pthread_cleanup_pop(!0); //非0 表示 pth_exit_deal 能被调用
+	printf("tid=%lu\n",pthread_self());
+	//pthread_exit(NULL);
+	return (void *)10;
+}
+void signal_fun(int signo){
+	if(SIGALRM == signo){
+		int i =0;
+		for (;i<SECON_PTH_NUMS;i++){
+			//pthread_cancel(glbva.pth_arg[i].tid);
+			glbva.pth_exit_flag[i]=PTHEXIT; //设置为退出状态
+		}
+	}else if(SIGINT == signo){
+		int ret=0;
+		ret=pthread_attr_destroy(&glbva.attr);
+		if(ret !=0) print_err("pthread_attr_destroy error ",__LINE__,ret);
+
+		exit(0);
+	}
+}
+void process_exit_deal(void){
+	printf("\nprocess exit\n");
+}
+
+int main(int argc, char const *argv[])
+{
+	//注册进程退出函数.exit正常终止时，弹栈调用
+	atexit(process_exit_deal);
+	pthread_t tid=0;
+	int i=0;
+	int ret =0;
+	int fd =0;
+	signal(SIGALRM,signal_fun);
+
+	fd=open("./file",O_RDWR|O_CREAT|O_TRUNC,0664);
+	if(fd ==-1) print_err("open file fail",__LINE__,errno);
+
+	//初始化attr设置一些基本的初始值
+	ret =pthread_attr_init(&glbva.attr);
+	if(ret !=0) print_err("pthread_attr_init error ",__LINE__,ret);
+	//设置分离属性
+	ret=pthread_attr_setdetachstate(&glbva.attr, PTHREAD_CREATE_DETACHED);
+	if(ret !=0) print_err("pthread_attr_setdetachstate error ",__LINE__,ret);
+
+	for(;i<SECON_PTH_NUMS;i++){
+		glbva.pth_arg[i].fd =fd;
+		glbva.pth_arg[i].pthno=i;
+		ret= pthread_create(&glbva.pth_arg[i].tid,&glbva.attr,pth_fun,(void *)&glbva.pth_arg[i]);
+		if(ret !=0 ) print_err("pthread_creat err",__LINE__,ret);
+		//pthread_detach(glbva.pth_arg[i].tid); //或者在次线程里写
+
+	}
+	signal(SIGINT,signal_fun);
+	
+	alarm(5);//不会阻塞
+	#if 0
+	void *retval =NULL;
+
+	for(i=0;i<SECON_PTH_NUMS;i++){
+		pthread_join(glbva.pth_arg[i].tid,&retval);//阻塞等待次线程结束，接收返回值
+		printf("retval =%ld\n",(long)retval );
+	}
+	#endif
+
+	while(1){
+		printf("main tid=%lu\n",pthread_self());
+		write(fd,"js",2);
+		write(fd,"css\n",4);
+		sleep(1);
+	}
+	return 0;
+}
